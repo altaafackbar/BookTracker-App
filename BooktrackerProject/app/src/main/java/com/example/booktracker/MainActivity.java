392d3e440514bfb9a5c4b63ec4c3db33fe99f04a
TheSTruong <svtruong@ualberta.ca>
2020-10-27 18:16:34 -0600

    Get textviews for displaying story 3.2.1. Waiting for classes to get properties

--- BooktrackerProject/app/src/main/java/com/example/booktracker/MainActivity.java
+++ BooktrackerProject/app/src/main/java/com/example/booktracker/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.booktracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    TextView descriptionBox = (TextView) (findViewById(R.id.descriptionTV));
+    TextView ownerBox = (TextView) (findViewById(R.id.ownerTV));
+    TextView statusBox = (TextView) (findViewById(R.id.statusTV));
+
+
 }
+
+T

f149d24d1c256c3b4b62a69e92dcdc94b0fff6b1
TheSTruong <svtruong@ualberta.ca>
2020-11-02 15:28:01 -0700

    Start and stop camerasource added. Get camera usage permission from user

--- BooktrackerProject/app/src/main/java/com/example/booktracker/ScanBarcodeActivity.java
+++ BooktrackerProject/app/src/main/java/com/example/booktracker/ScanBarcodeActivity.java
@@ -1,22 +1,30 @@
 package com.example.booktracker;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
+import java.io.IOException;
+
 public class ScanBarcodeActivity extends Activity {
     SurfaceView cameraPreview;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_barcode);
 
-        cameraPreview = (SurfaceView)findViewById(R.id.camera_preview);
+        cameraPreview = (SurfaceView) findViewById(R.id.camera_preview);
         createCameraSource();
     }
 
@@ -27,5 +35,35 @@ public class ScanBarcodeActivity extends Activity {
                 .setRequestedPreviewSize(1600, 1024)
                 .build();
 
+        cameraPreview.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(ScanBarcodeActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    cameraSource.start(cameraPreview.getHolder());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+        });
     }
 }

be350b41303ebe74a4338bab06c09de6e1a0aaf8
lclarke1 <lclarke1@ualberta.ca>
2020-11-02 21:45:27 -0700

    Updated ViewProfile Java Class and XML

---
+++ BooktrackerProject/app/src/main/java/com/example/booktracker/ViewProfileFragment.java
@@ -0,0 +1,45 @@
+package com.example.booktracker;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+public class ViewProfileFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        final View profile = inflater.inflate(R.layout.fragment_view_profile,container,false);
+        final Button back = profile.findViewById(R.id.view_profile_back_button);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(profile).navigate(R.id.action_viewProfileFragment_to_navigation_home);
+            }
+        });
+
+        final ListView bookList = profile.findViewById(R.id.view_profile_book_list);
+        bookList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(profile).navigate(R.id.action_viewProfileFragment_to_bookPageFragment);
+            }
+        });
+
+        return profile;
+    }
+}

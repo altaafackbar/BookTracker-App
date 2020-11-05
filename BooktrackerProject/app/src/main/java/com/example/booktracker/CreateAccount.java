a99e3d5bd59327c0cbefa698bf64557748560e19
Altaaf Ackbar <altaafackbar@hotmail.com>
2020-11-05 01:54:39 -0700

    Unique usernames, checks if username is taken

--- BooktrackerProject/app/src/main/java/com/example/booktracker/CreateAccount.java
+++ BooktrackerProject/app/src/main/java/com/example/booktracker/CreateAccount.java
@@ -1,14 +1,20 @@
 package com.example.booktracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -34,19 +40,36 @@ public class CreateAccount extends AppCompatActivity {
         });
     }
     private void new_user(){
-        String u_email = email.getText().toString();
+        final String u_email = email.getText().toString();
         String u_pass = password.getText().toString();
         String u_num = number.getText().toString();
-        HashMap<String, String> data = new HashMap<>();
+        final HashMap<String, String> data = new HashMap<>();
         data.put("UserEmail", u_email);
         data.put("UserPass", u_pass);
         data.put("UserNum", u_num);
         db = FirebaseFirestore.getInstance();
-        db.collection("Users").document(u_email).set(data);
-        MainActivity.current_user = u_email;
-        Intent sign_in = new Intent(this, MainScreen.class);
+        DocumentReference docIdRef = db.collection("Users").document(u_email);
+        docIdRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d("TAG", "Document exists!");
+                        Toast toast = Toast.makeText(getApplicationContext(), "Username unavailable, please choose another", Toast.LENGTH_SHORT);
+                        toast.show();
+                    } else {
+                        Log.d("TAG", "Document does not exist!");
+                        db.collection("Users").document(u_email).set(data);
+                        MainActivity.current_user = u_email;
+                        Intent sign_in = new Intent(getApplicationContext(), MainScreen.class);
+                        startActivity(sign_in);
+                    }
+                } else {
+                    Log.d("TAG", "Failed with: ", task.getException());
+                }
+            }
+        });
 
-
-        startActivity(sign_in);
     }
 }

8a4758319e2b2ea2211337ae23e5816d7bdce29c
unknown <cheming@ualberta.ca>
2020-11-06 10:29:55 -0800

    listview for request tab

--- BooktrackerProject/app/src/main/java/com/example/booktracker/ui/RequestTabFragment.java
+++ BooktrackerProject/app/src/main/java/com/example/booktracker/ui/RequestTabFragment.java
@@ -1,14 +1,26 @@
 package com.example.booktracker.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.booktracker.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +37,8 @@ public class RequestTabFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    //ArrayList<String> tempUnames = new ArrayList<String>(); //Temporary Usernames to add to list, to be replaced with requesters
 
     public RequestTabFragment() {
         // Required empty public constructor
@@ -51,6 +65,7 @@ public class RequestTabFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -61,6 +76,67 @@ public class RequestTabFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_request_tab, container, false);
+
+        ArrayList<String> tempUnames = new ArrayList<String>(); //Temporary Usernames to add to list, to be replaced with requesters
+        tempUnames.add("User1"); //Temporary Usernames to add to list, to be replaced with requesters
+        tempUnames.add("User2"); //Temporary Usernames to add to list, to be replaced with requesters
+        View view = inflater.inflate(R.layout.fragment_request_tab, container, false);
+        ListView listView = (ListView)view.findViewById(R.id.requestListView);
+        listView.setAdapter(new RequestListAdapter(getActivity(),R.layout.request_list_item,tempUnames));
+        return view;
     }
+
+
+    private class RequestListAdapter extends ArrayAdapter<String>{
+        private int layout;
+        public RequestListAdapter(@NonNull Context context, int resource, @NonNull List<String> objects) {
+            super(context, resource, objects);
+            layout = resource;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            ViewHolder mainViewHolder = null;
+            if(convertView==null){
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(layout, parent, false);
+                ViewHolder viewHolder = new ViewHolder();
+                viewHolder.profile_pic = (ImageView)convertView.findViewById(R.id.requester_profile_pic);
+                viewHolder.r_username = (TextView)convertView.findViewById(R.id.requester_username);
+                viewHolder.accept_btn = (Button)convertView.findViewById(R.id.request_accept_btn);
+                viewHolder.decline_btn = (Button)convertView.findViewById(R.id.request_decline_btn);
+
+                viewHolder.accept_btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getContext(),"Accepted",Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                viewHolder.decline_btn.setOnClickListener((new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getContext(),"Declined",Toast.LENGTH_SHORT).show();
+                    }
+                }));
+
+                convertView.setTag(viewHolder);
+            }
+            else{
+                mainViewHolder = (ViewHolder)convertView.getTag();
+                mainViewHolder.r_username.setText(getItem(position));
+            }
+            return convertView;
+        }
+    }
+    public class ViewHolder{
+        ImageView profile_pic;
+        TextView r_username;
+        Button accept_btn;
+        Button decline_btn;
+    }
+
+
+
 }

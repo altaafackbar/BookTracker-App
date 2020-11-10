4f0020ae7b93f5922fcbfb4c034653a72d835df9
unknown <cheming@ualberta.ca>
2020-11-10 14:25:04 -0800

    Listview for borrowing

--- BooktrackerProject/app/src/main/java/com/example/booktracker/ui/BookBorrowFragment.java
+++ BooktrackerProject/app/src/main/java/com/example/booktracker/ui/BookBorrowFragment.java
@@ -1,14 +1,27 @@
 package com.example.booktracker.ui;
 
+import android.content.Context;
+import android.media.Image;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.booktracker.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -61,6 +74,54 @@ public class BookBorrowFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_borrow, container, false);
+        ArrayList<String> tempUnames = new ArrayList<String>(); //Temporary Usernames to add to list, to be replaced with owners
+        tempUnames.add("User1"); //Temporary Usernames to add to list, to be replaced with owners
+        tempUnames.add("User2"); //Temporary Usernames to add to list, to be replaced with owners
+        View view = inflater.inflate(R.layout.fragment_book_borrow, container, false);
+        ListView listView = (ListView)view.findViewById(R.id.available_borrow_list);
+        listView.setAdapter(new BorrowListAdapter(getActivity(),R.layout.borrow_list_item,tempUnames));
+        return view;
+    }
+
+    private class BorrowListAdapter extends ArrayAdapter<String>{
+        private int layout;
+        public BorrowListAdapter(@NonNull Context context, int resource, @NonNull List<String> objects) {
+            super(context, resource, objects);
+            layout = resource;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            ViewHolder mainViewHolder = null;
+            if(convertView == null){
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(layout,parent,false);
+                ViewHolder viewHolder = new ViewHolder();
+                viewHolder.owner_pic = (ImageView)convertView.findViewById(R.id.borrow_book_owner_pic);
+                viewHolder.owner_name = (TextView)convertView.findViewById(R.id.borrow_book_owner_username);
+                viewHolder.owner_location = (TextView)convertView.findViewById(R.id.borrow_book_owner_location);
+                viewHolder.request_button = (Button)convertView.findViewById(R.id.borrow_request_button);
+                viewHolder.request_button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getContext(), "Request button clicked", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                convertView.setTag(viewHolder);
+            }
+            else{
+                mainViewHolder = (ViewHolder) convertView.getTag();
+                mainViewHolder.owner_name.setText(getItem(position));
+
+            }
+            return convertView;
+        }
+    }
+    public class ViewHolder{
+        ImageView owner_pic;
+        TextView owner_name;
+        TextView owner_location;
+        Button request_button;
     }
 }

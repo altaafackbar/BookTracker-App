a1ebc3aafb44bbc0239375c1ee1e762d4b1269a2
unknown <cheming@ualberta.ca>
2020-11-13 15:29:11 -0800

    Updated layout for fragment home

--- BooktrackerProject/app/src/main/java/com/example/booktracker/ui/home/HomeFragment.java
+++ BooktrackerProject/app/src/main/java/com/example/booktracker/ui/home/HomeFragment.java
@@ -15,9 +15,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.booktracker.Book;
 import com.example.booktracker.MainActivity;
 import com.example.booktracker.R;
+import com.example.booktracker.RecyclerViewAdapter;
 import com.example.booktracker.ScanBarcodeActivity;
 import com.example.booktracker.ViewProfileFragment;
 import com.google.android.gms.common.api.CommonStatusCodes;
@@ -30,6 +34,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 import static android.content.ContentValues.TAG;
@@ -42,12 +47,18 @@ public class HomeFragment extends Fragment {
     private String owner;
     public static String searchUser;
     private String status;
+    private String title;
+    private String author;
+    private String isbn;
+    private String bookImg;
+    RecyclerView myRecyclerview;
+    ArrayList<Book> bookList;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
-         final View root = inflater.inflate(R.layout.fragment_home, container, false);
+        final View root = inflater.inflate(R.layout.fragment_home, container, false);
         final TextView email = root.findViewById(R.id.email);
         /*
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -82,7 +93,40 @@ public class HomeFragment extends Fragment {
                 startActivityForResult(intent, 0);
             }
         });
+        bookList = new ArrayList<>();
+        myRecyclerview = (RecyclerView) root.findViewById(R.id.recyclerview_id_home);
+        final RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(getActivity(), bookList);
+        myRecyclerview.setLayoutManager(new GridLayoutManager(getActivity(), 3 ));
+        myRecyclerview.setAdapter(myAdapter);
 
+
+        db = FirebaseFirestore.getInstance();
+        db.collection("Users").document(MainActivity.current_user)
+                .collection("Books")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData().get("book"));
+                                Map<String, Object> book = (Map<String, Object>) document.getData().get("book");
+                                isbn = document.getId();
+                                title = (String) book.get("title");
+                                author = (String) book.get("author");
+                                status = (String)book.get("status");
+                                bookImg = (String) book.get("image");
+                                Book newBook = new Book(title, author, isbn,status, MainActivity.current_user);
+                                newBook.setImage(bookImg);
+                                bookList.add(newBook);
+
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                        myAdapter.notifyDataSetChanged();
+                    }
+                });
         return root;
     }
 

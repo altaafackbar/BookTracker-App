2003aa7ec5b3f75bf1601dbf93d47a51ad27ecc3
unknown <cheming@ualberta.ca>
2020-11-15 12:10:54 -0800

    Requested books added to requested list

--- BooktrackerProject/app/src/main/java/com/example/booktracker/ui/notifications/NotificationRequestedTabFragment.java
+++ BooktrackerProject/app/src/main/java/com/example/booktracker/ui/notifications/NotificationRequestedTabFragment.java
@@ -1,14 +1,38 @@
 package com.example.booktracker.ui.notifications;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.booktracker.Book;
+import com.example.booktracker.MainActivity;
 import com.example.booktracker.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static android.content.ContentValues.TAG;
 
 public class NotificationRequestedTabFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
@@ -19,6 +43,15 @@ public class NotificationRequestedTabFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private FirebaseFirestore db;
+    ArrayList<Book> bookList;
+    private String title;
+    private String author;
+    private String isbn;
+    private String status;
+    private String bookImg;
+    private String owner;
+    RequestedListAdapter myAdapter;
 
     //ArrayList<String> tempUnames = new ArrayList<String>(); //Temporary Usernames to add to list, to be replaced with requesters
 
@@ -54,8 +87,86 @@ public class NotificationRequestedTabFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        bookList = new ArrayList<>();
         View view = inflater.inflate(R.layout.fragment_notification_requested_tab, container, false);
         ListView listView = (ListView)view.findViewById(R.id.notificationRequestedListView);
+        myAdapter = new RequestedListAdapter(getActivity(), R.layout.requested_list_item, bookList);
+        listView.setAdapter(myAdapter);
+        db = FirebaseFirestore.getInstance();
+        db.collection("Users").document(MainActivity.current_user)
+                .collection("Requested Books")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData().get("book"));
+                                Map<String, Object> book = (Map<String, Object>) document.getData().get("book");
+                                isbn = document.getId();
+                                title = (String) book.get("title");
+                                author = (String) book.get("author");
+                                status = (String)book.get("status");
+                                bookImg = (String) book.get("image");
+                                owner = (String) book.get("owner");
+                                Book newBook = new Book(title, author, isbn, status, owner);
+                                newBook.setImage(bookImg);
+                                bookList.add(newBook);
+
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                        myAdapter.notifyDataSetChanged();
+                    }
+                });
         return view;
+    }
+
+    private class RequestedListAdapter extends ArrayAdapter<Book>{
+        private int layout;
+        public RequestedListAdapter(@NonNull Context context, int resource, @NonNull List<Book> objects) {
+            super(context, resource, objects);
+            layout = resource;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            RequestedViewHolder mainViewHolder = null;
+            if(convertView ==null){
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(layout, parent, false);
+                RequestedViewHolder viewHolder = new RequestedViewHolder();
+                viewHolder.title = (TextView) convertView.findViewById(R.id.requested_book_title);
+                viewHolder.description = (TextView) convertView.findViewById(R.id.requested_book_description);
+                viewHolder.status = (TextView) convertView.findViewById(R.id.requested_book_status);
+                viewHolder.owner = (TextView) convertView.findViewById(R.id.requested_book_owner);
+                viewHolder.book_pickup_button = (Button) convertView.findViewById(R.id.requested_book_pickup_button);
+                viewHolder.book_pickup_button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(getContext(), "Button Clicked", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                viewHolder.title.setText("Title: "+getItem(position).getTitle());
+                viewHolder.owner.setText("Owner: " + getItem(position).getOwner());
+                viewHolder.description.setText("Description: ");
+                viewHolder.status.setText("Status: ");
+
+                convertView.setTag(viewHolder);
+            }
+            else{
+                mainViewHolder = (RequestedViewHolder) convertView.getTag();
+            }
+            return convertView;
+        }
+    }
+    public class RequestedViewHolder{
+        TextView title;
+        TextView description;
+        TextView status;
+        TextView owner;
+        Button book_pickup_button;
     }
 }

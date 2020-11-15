314cfb163429dad40867fc146e3312745b047c7d
unknown <cheming@ualberta.ca>
2020-11-15 10:13:03 -0800

    Tabs for notification fragment

---
+++ BooktrackerProject/app/src/main/java/com/example/booktracker/ui/notifications/NotificationPageAdapter.java
@@ -0,0 +1,36 @@
+package com.example.booktracker.ui.notifications;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class NotificationPageAdapter extends FragmentPagerAdapter {
+    private int numOfTabs;
+
+    public NotificationPageAdapter(@NonNull FragmentManager fm, int numOfTabs) {
+        super(fm);
+        this.numOfTabs = numOfTabs;
+    }
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                return new NotificationRequestsTabFragment();
+            case 1:
+                return new NotificationRequestedTabFragment();
+            default:
+                return null;
+        }
+    }
+    @Override
+    public int getCount() {
+        return numOfTabs;
+    }
+
+    @Override
+    public int getItemPosition(@NonNull Object object) {
+        return POSITION_NONE;
+    }
+}
--- BooktrackerProject/app/src/main/java/com/example/booktracker/ui/notifications/NotificationsFragment.java
+++ BooktrackerProject/app/src/main/java/com/example/booktracker/ui/notifications/NotificationsFragment.java
@@ -11,25 +11,101 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.booktracker.R;
+import com.example.booktracker.TrackPageAdapter;
+import com.example.booktracker.ui.BookTrackerFragment;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
 
 public class NotificationsFragment extends Fragment {
+    public TextView back;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private TabItem requestsTab, requestedTab;
+    public NotificationPageAdapter pagerAdapter;
 
     private NotificationsViewModel notificationsViewModel;
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public NotificationsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment BookPageFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static NotificationsFragment newInstance(String param1, String param2) {
+        NotificationsFragment fragment = new NotificationsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
                 ViewModelProviders.of(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        View view = inflater.inflate(R.layout.fragment_notifications, container, false);
+        tabLayout = (TabLayout)view.findViewById(R.id.notification_tab_layout);
+        requestsTab = (TabItem)view.findViewById(R.id.requests_tab);
+        requestedTab = (TabItem)view.findViewById(R.id.requested_tab);
+        viewPager = view.findViewById(R.id.notification_viewpager);
+
+        pagerAdapter = new NotificationPageAdapter(getFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if(tab.getPosition() == 0){
+                    pagerAdapter.notifyDataSetChanged();
+                }
+                else if (tab.getPosition() == 1){
+                    pagerAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
-        return root;
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        return view;
     }
 }

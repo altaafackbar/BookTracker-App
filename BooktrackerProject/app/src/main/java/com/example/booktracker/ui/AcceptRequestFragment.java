55b497fb68cbd5e2718d3c3a7dc19343d8fbd31c
unknown <cheming@ualberta.ca>
2020-11-19 16:28:36 -0800

    List of requests received

--- BooktrackerProject/app/src/main/java/com/example/booktracker/ui/AcceptRequestFragment.java
+++ BooktrackerProject/app/src/main/java/com/example/booktracker/ui/AcceptRequestFragment.java
@@ -3,6 +3,7 @@ package com.example.booktracker.ui;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,17 +18,34 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.booktracker.Book;
+import com.example.booktracker.MainActivity;
 import com.example.booktracker.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
+import static android.content.ContentValues.TAG;
 
 public class AcceptRequestFragment extends Fragment {
     public TextView back;
     private String title;
     private String author;
+    ArrayList<String> requesterList;
+    private String owner;
+    private String requestStatus;
+
     private String status;
+    private String current_isbn;
     private String isbn;
     private String img;
     private Bitmap bitmap;
@@ -77,15 +95,58 @@ public class AcceptRequestFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        final View view = inflater.inflate(R.layout.fragment_accept_request, container, false);
+        View view = inflater.inflate(R.layout.fragment_accept_request, container, false);
+        requesterList = new ArrayList<>();
         isbn = getArguments().getString("isbn");
-        Toast.makeText(getContext(),isbn,Toast.LENGTH_SHORT).show();
-        ArrayList<String> tempUnames = new ArrayList<String>(); //Temporary Usernames to add to list, to be replaced with requesters
-        tempUnames.add("User1"); //Temporary Usernames to add to list, to be replaced with requesters
-        tempUnames.add("User2"); //Temporary Usernames to add to list, to be replaced with requesters
+        db = FirebaseFirestore.getInstance();
+        final CollectionReference collectionReference = db.collection("Users");
+
+
         ListView listView = (ListView)view.findViewById(R.id.requests_accept_list);
-        listView.setAdapter(new RequestListAdapter(getActivity(),R.layout.request_list_item,tempUnames));
+        final RequestListAdapter myAdapter = (new RequestListAdapter(getActivity(),R.layout.request_list_item, requesterList));
+        listView.setAdapter(myAdapter);
+
+
+        collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
+                requesterList.clear();
+                final ArrayList<String> userEmailList = new ArrayList<>();
+                for(QueryDocumentSnapshot doc: queryDocumentSnapshots)
+                {
+                    userEmailList.add((String) doc.getData().get("UserEmail"));
+                }
+                for(final String uEmail:userEmailList){
+                    if (!uEmail.equals(MainActivity.current_user)) {
+                        db.collection("Users").document(uEmail)
+                                .collection("Requested Books")
+                                .get()
+                                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                        if (task.isSuccessful()) {
+                                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                                Log.d(TAG, document.getId() + " => " + document.getData().get("book"));
+                                                Map<String, Object> book = (Map<String, Object>) document.getData().get("book");
+                                                requestStatus = (String) book.get("requestStatus");
+                                                owner = (String)book.get("owner");
+                                                current_isbn = (String)book.get("isbn");
+                                                if (owner != null && requestStatus != null && requestStatus.equals("Pending Request") && owner.equals(MainActivity.current_user) && current_isbn.equals(isbn)) {
+                                                    requesterList.add(uEmail);
+                                                }
+                                            }
+                                        } else {
+                                            Log.d(TAG, "Error getting documents: ", task.getException());
+                                        }
+                                        myAdapter.notifyDataSetChanged();
+                                    }
+                                });
+                    }
+                }
+            }
+        });
+
+
         return view;
     }
 
@@ -122,6 +183,7 @@ public class AcceptRequestFragment extends Fragment {
                         Toast.makeText(getContext(),"Declined",Toast.LENGTH_SHORT).show();
                     }
                 }));
+                viewHolder.r_username.setText(getItem(position));
 
                 convertView.setTag(viewHolder);
             }
